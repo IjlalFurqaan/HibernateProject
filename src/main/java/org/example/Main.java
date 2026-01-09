ac5e12d615393625909049eccf030a49eb2afb59
Ijlal Furqaan <ijlalfurqaan5@gmail.com>
2026-01-09 18:55:01 +0100

    s namings

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Student s1 = new Student();
+        s1.setRollNo(101);
+        s1.setsName("John Doe");
+        s1.setsAge(20);
+
+        System.out.println("Roll No: " + s1.getRollNo());
+        System.out.println("Student Name: " + s1.getsName());
+        System.out.println("Student Age: " + s1.getsAge());
     }
 }
